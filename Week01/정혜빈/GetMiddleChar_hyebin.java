222a3d4cd67814fbb7ff0e899dee770be4e0a500
hyebinJeong <beeni1211@naver.com>
2026-01-11 00:10:15 +0900

    test: test commit

---
+++ Week01/정혜빈/GetMiddleChar_hyebin.java
@@ -0,0 +1,9 @@
+package Week01;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12903
+
+public class GetMiddleChar_hyebin {
+    public static void main(String[] args) {
+        // test 작성
+    }
+}
