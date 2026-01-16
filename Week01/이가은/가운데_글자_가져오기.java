55c096aa55cc012067292f5a70572ec7f619d4dc
이_가은 <93564657+Lee-gaeun@users.noreply.github.com>
2026-01-16 09:32:07 +0900

    Add solution for retrieving middle character of string
    Implement solution to retrieve the middle character(s) of a string.

--- Week01/이가은/가운데_글자_가져오기.java
+++ Week01/이가은/가운데_글자_가져오기.java
@@ -2,8 +2,21 @@ package Week01;
 
 // https://school.programmers.co.kr/learn/courses/30/lessons/12903
 
-public class 가운데_글자_가져오기 {
-    public static void main(String[] args) {
-
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int N = s.length();
+        
+        if (N % 2 == 0) {
+     
+            answer = s.substring(N / 2 - 1, N / 2 + 1);
+        } else {
+            
+            answer = s.charAt(N / 2) + "";
+        }
+        
+        return answer;
     }
 }
+}
+
