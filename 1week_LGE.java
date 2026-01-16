ffb0e4d1f038caf1d6944608e61c8b49496c9fc0
이_가은 <93564657+Lee-gaeun@users.noreply.github.com>
2026-01-16 09:28:57 +0900

    add 1week code

---
+++ 1week_LGE.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int N = s.length();
+        
+        if (N % 2 == 0) {
+     
+            answer = s.substring(N / 2 - 1, N / 2 + 1);
+        } else {
+            
+            answer = s.charAt(N / 2) + "";
+        }
+        
+        return answer;
+    }
+}
